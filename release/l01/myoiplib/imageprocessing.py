b91a8039d7daca129ccf5077650c6f93ce077c26
till <till@fresnel.tek.sdu.dk>
2025-09-02 11:31:47 +0200

    Apply .gitignore to keep only my_folder

---
+++ release/l01/myoiplib/imageprocessing.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""This library has been developed by 
+
+Till Leissner (till@mci.sdu.dk) 
+
+as part of the course Optimisation and Image processing, 2023
+at the University of Southern Denmark.
+
+Version: 0.1
+"""
+
+
+import numpy as np                  # for all kinds of (accelerated) matrix / numerical operations
+
+def hello():
+    print('Hello world!')
+
+
+
+
+
+
+
+
+
+
+    
